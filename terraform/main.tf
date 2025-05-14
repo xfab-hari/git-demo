fb5c1388aafa01bac31f798d6c5394d67adb43fa
xfab-hari <hari@xfabric.ai>
2025-05-14 11:25:44 -0700

    added initial terraform example

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,20 +1,35 @@
-terraform {
- required_providers {
-   aws = {
-     source = "hashicorp/aws"
-   }
- }
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+  owners = ["099720109477"] # Canonical
 }
-
 provider "aws" {
- region = "us-west-2"
+  region = "us-east-1"
 }
- 
-resource "aws_instance" "test_instance" {
- ami           = "ami-830c94e3"
- instance_type = "t2.nano"
- tags = {
-   Name = "test_instance"
- }
+# EC2 Instance
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t3.micro"
+  tags = {
+    Name = "Terraform-VM"
+  }
 }
-
+# RDS Database (PostgreSQL)
+resource "aws_db_instance" "default" {
+  allocated_storage    = 10
+  db_name              = "mydb"
+  engine               = "mysql"
+  engine_version       = "8.0"
+  instance_class       = "db.t3.micro"
+  username             = "foo"
+  password             = "foobarbaz"
+  parameter_group_name = "default.mysql8.0"
+  skip_final_snapshot  = true
+}
