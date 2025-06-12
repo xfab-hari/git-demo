15e3657f64263a7b2301e235ed40961ca23d2c13
xfab-hari <hari@xfabric.ai>
2025-06-12 09:27:47 -0700

    added tf files

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,91 +1,69 @@
-variable "project_id" {}
-variable "region" {}
-variable "zone" {}
+# Copyright (c) HashiCorp, Inc.
+# SPDX-License-Identifier: MPL-2.0
 
-variable "existing_service_account_email" {}
-
-# Bucket
-variable "bucket_name" {}
-variable "bucket_location" {}
-variable "bucket_iam_role" {}
-
-# VM
-variable "vm_name" {}
-variable "vm_machine_type" {}
-variable "vm_boot_image" {}
-variable "network" {}
-variable "vm_scopes" {
-  type = list(string)
-}
-variable "vm_tags" {
-  type = list(string)
+variable "aws_region" {
+  description = "AWS region"
+  type        = string
+  default     = "us-east-1"
 }
 
-# GKE Cluster
-variable "gke_cluster_name" {}
-variable "gke_machine_type" {}
+variable "vpc_cidr_block" {
+  description = "CIDR block for VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
 
-# Cloud Function
-variable "cloud_function_name" {}
-variable "cloud_function_source_archive_object" {}
+variable "public_subnet_cidr_blocks" {
+  description = "Available cidr blocks for public subnets"
+  type        = list(string)
+  default = [
+    "10.0.1.0/24",
+    "10.0.2.0/24",
+    "10.0.3.0/24",
+    "10.0.4.0/24",
+    "10.0.5.0/24",
+    "10.0.6.0/24",
+    "10.0.7.0/24",
+    "10.0.8.0/24"
+  ]
+}
+
+variable "private_subnet_cidr_blocks" {
+  description = "Available cidr blocks for private subnets"
+  type        = list(string)
+  default = [
+    "10.0.101.0/24",
+    "10.0.102.0/24",
+    "10.0.103.0/24",
+    "10.0.104.0/24",
+    "10.0.105.0/24",
+    "10.0.106.0/24",
+    "10.0.107.0/24",
+    "10.0.108.0/24"
+  ]
+}
+
+variable "instances_per_subnet" {
+  description = "Number of EC2 instances per private subnet"
+  type        = number
+  default     = 1
+}
+
+variable "instance_type" {
+  description = "Instance type for EC2 instances"
+  type        = string
+  default     = "t2.micro"
+}
 
 
-# variable "project_id" {
-#   type = string
-# }
+variable "db_username" {
+  description = "Database admin username"
+  type        = string
+  sensitive   = true
+}
 
-# variable "region" {
-#   type = string
-# }
-
-# variable "zone" {
-#   type = string
-# }
-
-# variable "service_account_id" {
-#   type = string
-# }
-
-# variable "service_account_display_name" {
-#   type = string
-# }
-
-# variable "bucket_name" {
-#   type = string
-# }
-
-# variable "bucket_location" {
-#   type = string
-# }
-
-# variable "bucket_iam_role" {
-#   type = string
-# }
-
-# variable "vm_name" {
-#   type = string
-# }
-
-# variable "vm_machine_type" {
-#   type = string
-# }
-
-# variable "vm_boot_image" {
-#   type = string
-# }
-
-# variable "network" {
-#   type = string
-# }
-
-# variable "vm_scopes" {
-#   type = list(string)
-# }
-
-# variable "vm_tags" {
-#   type = list(string)
-# }
-# variable "existing_service_account_email" {
-#   type = string
-# }
-
+variable "db_password" {
+  description = "Database admin password"
+  type        = string
+  sensitive   = true
+}
